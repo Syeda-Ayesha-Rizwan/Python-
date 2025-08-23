823db5702c4ca690b392d1bc332087a731736fa3
Syeda-Ayesha-Rizwan <rizwanayesha588@gmail.com>
2025-08-24 02:05:49 +0500

    data types

---
+++ dataType.py
@@ -0,0 +1,44 @@
+x = 10 
+age = 18
+name = "ayesha"
+print(type(x))
+print(type(age))
+print(type(name))
+# numeric type 
+
+price = 99.9 
+print(price)
+print(type(price))
+
+#complex
+z= 3 +4j
+print(z.real)
+print(z.imag)
+
+# string
+name = "ayesha"
+print(type(name))
+#boolean
+is_student = True
+print(is_student)
+print(type(is_student))
+ 
+ #none type
+ #x = None
+print(x)
+#print(type(x))
+
+#collections
+#list
+fruits = ["apple" , "orange" , "mango"]
+print(fruits)
+
+# tuple
+colors = ("red" , "pink "  , "blue " "blue")
+print(colors)
+#set
+nums = {1 , 3 , 2 , 2 }
+print(nums)
+#dictionary 
+student = {"name" : "Ayesha" , "age" : " 18"}
+print(student)
