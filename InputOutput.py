36c9b40139d483b7b669bfc6a93b0bfd289b0340
Syeda-Ayesha-Rizwan <rizwanayesha588@gmail.com>
2025-08-24 23:48:37 +0500

    input output

---
+++ InputOutput.py
@@ -0,0 +1,27 @@
+print("hello world")
+print("My name is " , "Ayesha")
+print("My name is ")
+print("Ayesha")
+#seperator 
+print("Apple" , "banana " , "mango " , sep="|")
+#end:
+print("this is my first python " , end=" ")
+print("program ")
+#escape sequences 
+#\n , \t
+print("this is a \n new line ")
+#\t new tab 
+print("tab\tspace ")
+
+
+#input 
+# input()
+#name = input("Enter your name : ")
+#print("hello " , name )
+name = input("Enter your name : ")
+age = input("Enter your age : ")
+print(f"Your name is {name } and your age is {age }")
+
+
+
+
